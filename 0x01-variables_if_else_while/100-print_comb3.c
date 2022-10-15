3e8ac00bc08f5ffe4e3417e67428bcf4892c594d
BABALOBI GBOLAHAN <gbolahanbabalobi@gmail.com>
2022-10-15 05:43:18 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,4 +5,26 @@
  * Return: 0
  */
 int main(void)
+{
+	int a, b;
 
+	for (a = '0'; b <= '9'; c++)
+	{
+		for (a = '0'; b <= '9'; C++)
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+
+				if (a == '8' && b == '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
