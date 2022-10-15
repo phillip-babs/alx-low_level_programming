89a11a27d473151f5820499e1c4c3a47760084f9
BABALOBI GBOLAHAN <gbolahanbabalobi@gmail.com>
2022-10-15 04:20:41 -0700

    changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		if (num != 9)
+		if (num != '9')
 		{
 			putchar(',');
 			putchar(' ');
