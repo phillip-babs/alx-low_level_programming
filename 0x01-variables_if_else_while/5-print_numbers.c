1553768de95295bac53ed76dd76890a6b1f80917
BABALOBI GBOLAHAN <gbolahanbabalobi@gmail.com>
2022-10-15 02:33:26 -0700

    task 3-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting with 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lower case alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	int la;
+
+	for (la = z; la >= a; la--)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
